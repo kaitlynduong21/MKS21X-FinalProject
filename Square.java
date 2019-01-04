de34a800caaf1273bd61cddf3cbb26e5d56a25f9
kduong@stuy.edu <kduong@stuy.edu>
2019-01-03 19:12:53 -0500

    Added constructor and isFilledIn method to test branching

--- Square.java
+++ Square.java
@@ -6,9 +6,20 @@ public class Square  {
 
   private int ycor;
 
+  private boolean showNumber;
+
   public Square(int number, int x, int y) {
-	   num = number;
+     num = number;
      xcor = x;
      ycor = y;
+
+  }
+
+  public int isFilledIn() {
+    if (showNumber == true) {
+      return num;
+    } else {
+      return -1;
+    }
   }
 }
