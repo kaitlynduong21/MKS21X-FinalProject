e53a12dc573a0dac723f24ca1030fcbe888121f5
kduong@stuy.edu <kduong@stuy.edu>
2019-01-05 13:53:47 -0500

    Working on toString, able to get it to work for the answer key but not the puzzle, getting nullpointerexceptions with the puzzle

--- Driver.java
+++ Driver.java
@@ -1,6 +1,19 @@
 public class Driver{
   public static void main(String[]args){
-    Sudoku newBoard = new Sudoku(9,9);
+    int[][] easy = new int[][]{
+      {2, 9, 6, 3, 1, 8, 5, 7, 4},
+      {5, 8, 4, 9, 7, 2, 6, 1 ,3},
+      {7, 1, 3, 6, 4, 5, 2, 8, 9},
+      {6, 2, 5, 8, 9, 7, 3, 4, 1},
+      {9, 3, 1, 4, 2, 6, 8, 5, 7},
+      {4, 7, 8, 5, 3, 1, 9, 2, 6},
+      {1, 6, 7, 2, 5, 3, 4, 9, 8},
+      {8, 5, 9, 7, 6, 4, 1, 3, 2},
+      {3, 4, 2, 1, 8, 9, 7, 6, 5},
+    };
+    Sudoku newBoard = new Sudoku(easy);
+    System.out.println(easy);
+    System.out.println(easy[0][0]);
     System.out.println(newBoard);
   }
 }
--- Sudoku.java
+++ Sudoku.java
@@ -1,45 +1,46 @@
 import java.util.Random;
 public class Sudoku{
-  private String[][] puzzle;
+  private int[][] puzzle;
 
-  private String[][] answer;
+  private int[][] answer;
 
-  private String[][] myBoard;
+  private int[][] myBoard;
 
   private String difficulty;
 
   private int seed;
 
-  private Random rand;
+  //private Random randgen;
 
-  public Sudoku(String[][] nums){
+  public Sudoku(int[][] nums){
     answer = nums;
-    puzzle = new String[nums.length][nums[0].length];
+    puzzle = new int[nums.length][nums[0].length];
     Random randgen = new Random();
     for (int x = 0; x < nums.length; x ++ ) {
       for (int y = 0; y <nums[0].length; y ++) {
         int rand = randgen.nextInt() % 4;
-        if (rand == 1) {
-          puzzle[x][y] = answer[x][y];
-        } else {
-          puzzle[x][y] = " ";
-        }
+        //if (rand == 1) {
+          puzzle[x][y] = nums[x][y];
+        /*} else {
+          puzzle[x][y] = -1;
+        }*/
       }
     }
     puzzle = myBoard;
   }
+
   public String toString(){
     String newstr = "";
-    for(int i = 1; i <= myBoard.length; i ++){
-      for(int a = 1; a <= myBoard[0].length + 3; a ++){
-        if(a % 4 == 0){
-          newstr += "|" + myBoard[i][a].isFilledIn() + " ";
+    for (int x = 0; x < answer.length; x ++ ) {
+      for (int y = 0; y < answer[0].length; y ++) {
+        if(y % 3 == 2){
+          newstr += "|" + answer[x][y] + "| ";
         }
         else{
-        newstr+= "|" + myBoard[i][a].isFilledIn();
+        newstr+= "|" + answer[x][y];
         }
       }
-      if(i % 3 == 0){
+      if(x % 3 == 2){
         newstr += "\n";
       }
       newstr+= "\n";
